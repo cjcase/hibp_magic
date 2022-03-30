d46bd60760a0aaedf8b7af0cffa19eea1ff99b6c
Cj Case <cj@abysmal.mx>
2022-03-30 14:30:19 +0300

    added license

--- hibp_magic.py
+++ hibp_magic.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+
 #
 # HaveIBeenPwned API Magic - Quick and dirty
 #                                  by cjcase
+
+#    hibp_magic - Quick and dirty HaveIBeenPwned API v3 interface
+#    Copyright (C) 2022 cjcase
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see https://www.gnu.org/licenses/
 
 import requests
 import argparse
